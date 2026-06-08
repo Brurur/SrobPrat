529e3ad76abdfb05917176ed3f2765846350808d
Brurur <caesarrianlanang17@gmail.com>
2026-06-08 10:05:14 +0700

    Visualisasi Data

--- Script/VisualisasiData.R
+++ Script/VisualisasiData.R
@@ -1,5 +1,6 @@
 library(readr)
 library(ggplot2)
+library(dplyr)
 
 data_wilayah <- read.csv(
   "Dataset/pembangunan_wilayah_missing_outlier.csv"
@@ -34,5 +35,60 @@ ggplot(data_provinsi,
     title = "Ranking IPM Provinsi",
     x = "Provinsi",
     y = "IPM"
+  ) +
+  theme_minimal()
+
+# 3. Geom Point Pendidikan dengan Harapan Hidup
+
+ggplot(
+  data_wilayah,
+  aes(
+    x = rata_lama_sekolah,
+    y = harapan_hidup
+  )
+) +
+  geom_point(
+    alpha = 0.7,
+    size = 2,
+    color = "#2C7FB8"
+  ) +
+  labs(
+    title = "Pendidikan dan Harapan Hidup",
+    subtitle = "Wilayah dengan pendidikan lebih tinggi cenderung memiliki usia harapan hidup lebih tinggi",
+    x = "Rata-rata Lama Sekolah (tahun)",
+    y = "Harapan Hidup (tahun)"
+  ) +
+  theme_minimal(base_size = 12)
+
+# 4. Tren DKI Jakarta dari Tahun ke Tahun
+dki <- data_wilayah %>%
+  filter(provinsi == "DKI Jakarta")
+
+ggplot(dki, aes(x = factor(tahun), y = ipm)) +
+  geom_boxplot(fill = "skyblue") +
+  stat_summary(
+    fun = median,
+    geom = "point",
+    size = 3,
+    shape = 18
+  ) +
+  labs(
+    title = "Distribusi IPM DKI Jakarta per Tahun",
+    x = "Tahun",
+    y = "IPM"
+  ) +
+  theme_minimal()
+
+# 5. Hubungan Pengangguran dan Kemiskinan
+ggplot(
+  data_wilayah,
+  aes(x = pengangguran, y = kemiskinan)
+) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(
+    title = "Hubungan Pengangguran dan Kemiskinan",
+    x = "Pengangguran (%)",
+    y = "Kemiskinan (%)"
   ) +
   theme_minimal()
