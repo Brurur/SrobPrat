c311033f0ae46e360f9d687d76104a8876295323
inkAja <mdghifari17@gmail.com>
2026-06-08 11:44:30 +0700

    Pencarian Outlier

--- Script/AnalisisOutlier.R
+++ Script/AnalisisOutlier.R
@@ -18,9 +18,11 @@ library(dplyr)
 #===============================================
 # BOXPLOT & IQR
 #===============================================
-# memilih data yang ingin ditampilkan (numeric aja)
 # fungsi iqr 
-hitung_iqr <- function(data_kolom, nama_variabel) {
+hitung_iqr <- function(data_input, nama_kolom, nama_variabel) {
+  # Ambil data kolom secara dinamis dari dataset utama
+  data_kolom <- data_input[[nama_kolom]]
+  
   # Q1, Q3, dan IQR
   q1 <- quantile(data_kolom, 0.25, na.rm = TRUE)
   q3 <- quantile(data_kolom, 0.75, na.rm = TRUE)
@@ -34,8 +36,10 @@ hitung_iqr <- function(data_kolom, nama_variabel) {
   jumlah_outlier_atas <- sum(data_kolom > batas_atas, na.rm = TRUE)
   jumlah_outlier_bawah <- sum(data_kolom < batas_bawah, na.rm = TRUE)
   
-  # Cetak Hasilnya
+  # Cetak Hasil Ringkasan Statistik
+  cat("===================================================\n")
   cat("=== ANALISIS IQR:", nama_variabel, "===\n")
+  cat("===================================================\n")
   cat("Kuartil 1 (Q1)       :", q1, "\n")
   cat("Kuartil 3 (Q3)       :", q3, "\n")
   cat("Nilai IQR            :", iqr_value, "\n")
@@ -43,11 +47,33 @@ hitung_iqr <- function(data_kolom, nama_variabel) {
   cat("Batas Normal Atas    :", batas_atas, "\n")
   cat("Jumlah Outlier Atas  :", jumlah_outlier_atas, "data\n")
   cat("Jumlah Outlier Bawah :", jumlah_outlier_bawah, "data\n")
-  cat("---------------------------------------------------\n\n")
+  cat("---------------------------------------------------\n")
+  
+  # Filter data oknum outlier
+  outlier_data <- data_input %>% 
+    filter(!!sym(nama_kolom) > batas_atas | !!sym(nama_kolom) < batas_bawah) %>% 
+    select(provinsi, tahun, !!sym(nama_kolom)) %>% 
+    arrange(desc(!!sym(nama_kolom))) # Urutkan dari yang paling ekstrem
+  
+  # Cetak jumlah total outlier
+  cat("Total Outlier Ditemukan:", nrow(outlier_data), "data\n")
+  cat("---------------------------------------------------\n")
+  
+  # --- 3. TAMPILKAN DAFTAR OKNUM OUTLIER ---
+  if(nrow(outlier_data) > 0) {
+    cat("Daftar Data Outlier (Menampilkan hingga 10 teratas):\n")
+    print(head(outlier_data, 10))
+  } else {
+    cat("Tidak ada data yang menjadi outlier.\n")
+  }
+  cat("\n\n")
 }
+
+# memilih data yang ingin ditampilkan (numeric aja)
 df_indikator <- data %>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
+
 # bagi menjadi 3 baris 3 kolom
 par(mfrow=c(3,3), mar=c(2, 2, 2, 1))
 
@@ -55,97 +81,105 @@ par(mfrow=c(3,3), mar=c(2, 2, 2, 1))
 for(i in 1:ncol(df_indikator)) {
   boxplot(df_indikator[[i]], main=colnames(df_indikator)[i], col="skyblue")
 }
-hitung_iqr(data$pdrb_perkapita, "PDRB PER KAPITA")
-hitung_iqr(data$kemiskinan, "KEMISKINAN")
-hitung_iqr(data$pengangguran, "PENGANGGURAN")
 
+# IQR untuk menghitung dan mencari siapa outliernya
+hitung_iqr(data, "pdrb_perkapita", "PDRB PER KAPITA")
+hitung_iqr(data, "kemiskinan", "KEMISKINAN")
+hitung_iqr(data, "pengangguran", "PENGANGGURAN")
 
 # Memisahkan dataset utama menjadi 5 dataset mandiri
 # data 2020
-data_2020 <- data %>% filter(tahun == 2020)  %>% 
+data_2020 <- data %>% filter(tahun == 2020) 
+data_2020_number <- data_2020 %>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
 par(mfrow = c(3, 3), mar = c(2, 2, 3, 1), oma = c(0, 0, 4, 0))
-data_2020$pdrb_perkapita
-for(i in 1:ncol(data_2020)) {
-  boxplot(data_2020[[i]], main = colnames(data_2020)[i], col = "green")
+
+for(i in 1:ncol(data_2020_number)) {
+  boxplot(data_2020_number[[i]], main = colnames(data_2020)[i], col = "green")
 }
 mtext("ANALISIS OUTLIER INDIKATOR DAERAH TAHUN 2020", 
       side = 3,
       outer = TRUE,   
       cex = 1,  
       font = 2)
-hitung_iqr(data_2020$pdrb_perkapita, "PDRB PER KAPITA Tahun 2020")
-hitung_iqr(data_2020$kemiskinan, "KEMISKINAN Tahun 2020")
-hitung_iqr(data_2020$pengangguran, "PENGANGGURAN Tahun 2020")
+hitung_iqr(data_2020,"pdrb_perkapita", "PDRB PER KAPITA Tahun 2020")
+hitung_iqr(data_2020,"kemiskinan", "KEMISKINAN Tahun 2020")
+hitung_iqr(data_2020,"pengangguran", "PENGANGGURAN Tahun 2020")
 
 # data 2021
-data_2021 <- data %>% filter(tahun == 2021)  %>% 
+data_2021 <- data %>% filter(tahun == 2021)  
+data_2021_number <- data_2021%>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
 par(mfrow = c(3, 3), mar = c(2, 2, 3, 1), oma = c(0, 0, 4, 0))
 
-for(i in 1:ncol(data_2021)) {
-  boxplot(data_2021[[i]], main=colnames(data_2021)[i], col="red")
+for(i in 1:ncol(data_2021_number)) {
+  boxplot(data_2021_number[[i]], main=colnames(data_2021)[i], col="red")
 }
 mtext("ANALISIS OUTLIER INDIKATOR DAERAH TAHUN 2021", 
       side = 3,
       outer = TRUE,   
       cex = 1,  
       font = 2)
-hitung_iqr(data_2021$pdrb_perkapita, "PDRB PER KAPITA Tahun 2021")
-hitung_iqr(data_2021$kemiskinan, "KEMISKINAN Tahun 2021")
-hitung_iqr(data_2021$pengangguran, "PENGANGGURAN Tahun 2021")
+hitung_iqr(data_2021,"pdrb_perkapita", "PDRB PER KAPITA Tahun 2021")
+hitung_iqr(data_2021,"kemiskinan", "KEMISKINAN Tahun 2021")
+hitung_iqr(data_2021,"pengangguran", "PENGANGGURAN Tahun 2021")
 
 # data 2022
-data_2022 <- data %>% filter(tahun == 2022)  %>% 
+data_2022 <- data %>% filter(tahun == 2022) 
+data_2022_number <- data_2022 %>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
+data_2022
 par(mfrow = c(3, 3), mar = c(2, 2, 3, 1), oma = c(0, 0, 4, 0))
-for(i in 1:ncol(data_2022)) {
-  boxplot(data_2022[[i]], main=colnames(data_2022)[i], col="purple")
+for(i in 1:ncol(data_2022_number)) {
+  boxplot(data_2022_number[[i]], main=colnames(data_2022)[i], col="purple")
 }
 mtext("ANALISIS OUTLIER INDIKATOR DAERAH TAHUN 2022", 
       side = 3,
       outer = TRUE,   
       cex = 1,  
       font = 2)
-hitung_iqr(data_2022$pdrb_perkapita, "PDRB PER KAPITA Tahun 2022")
-hitung_iqr(data_2022$kemiskinan, "KEMISKINAN Tahun 2022")
-hitung_iqr(data_2022$pengangguran, "PENGANGGURAN Tahun 2022")
+hitung_iqr(data_2022,"pdrb_perkapita", "PDRB PER KAPITA Tahun 2022")
+hitung_iqr(data_2022,"kemiskinan", "KEMISKINAN Tahun 2022")
+hitung_iqr(data_2022,"pengangguran", "PENGANGGURAN Tahun 2022")
 
 # data 2023
-data_2023 <- data %>% filter(tahun == 2023)  %>% 
+data_2023 <- data %>% filter(tahun == 2023)
+data_2023_number <- data_2023 %>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
+data_2023_number
 par(mfrow = c(3, 3), mar = c(2, 2, 3, 1), oma = c(0, 0, 4, 0))
-for(i in 1:ncol(data_2023)) {
-  boxplot(data_2023[[i]], main=colnames(data_2023)[i], col="yellow")
+for(i in 1:ncol(data_2023_number)) {
+  boxplot(data_2023_number[[i]], main=colnames(data_2023)[i], col="yellow")
 }
 mtext("ANALISIS OUTLIER INDIKATOR DAERAH TAHUN 2023", 
       side = 3,
       outer = TRUE,   
       cex = 1,  
       font = 2)
-hitung_iqr(data_2023$pdrb_perkapita, "PDRB PER KAPITA Tahun 2023")
-hitung_iqr(data_2023$kemiskinan, "KEMISKINAN Tahun 2023")
-hitung_iqr(data_2023$pengangguran, "PENGANGGURAN Tahun 2023")
+hitung_iqr(data_2023,"pdrb_perkapita", "PDRB PER KAPITA Tahun 2023")
+hitung_iqr(data_2023,"kemiskinan", "KEMISKINAN Tahun 2023")
+hitung_iqr(data_2023,"pengangguran", "PENGANGGURAN Tahun 2023")
 
 # data 2024
-data_2024 <- data %>% filter(tahun == 2024)  %>% 
+data_2024 <- data %>% filter(tahun == 2024) 
+data_2024_number <- data_2024 %>% 
   select(pdrb_perkapita, kemiskinan, pengangguran, ipm, 
          harapan_hidup, rata_lama_sekolah, akses_internet, jalan_baik, air_bersih)
 par(mfrow = c(3, 3), mar = c(2, 2, 3, 1), oma = c(0, 0, 4, 0))
-for(i in 1:ncol(data_2024)) {
-  boxplot(data_2024[[i]], main=colnames(data_2024)[i], col="skyblue")
+for(i in 1:ncol(data_2024_number)) {
+  boxplot(data_2024_number[[i]], main=colnames(data_2024)[i], col="blue")
 }
 mtext("ANALISIS OUTLIER INDIKATOR DAERAH TAHUN 2024", 
       side = 3,
       outer = TRUE,   
       cex = 1,  
       font = 2)
-hitung_iqr(data_2024$pdrb_perkapita, "PDRB PER KAPITA Tahun 2024")
-hitung_iqr(data_2024$kemiskinan, "KEMISKINAN Tahun 2024")
-hitung_iqr(data_2024$pengangguran, "PENGANGGURAN Tahun 2024")
+hitung_iqr(data_2024,"pdrb_perkapita", "PDRB PER KAPITA Tahun 2024")
+hitung_iqr(data_2024,"kemiskinan", "KEMISKINAN Tahun 2024")
+hitung_iqr(data_2024,"pengangguran", "PENGANGGURAN Tahun 2024")
 
 
